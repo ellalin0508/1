684c40f77d2955f5849497cb25621b9f46f3b12d
loisfeng1220 <loisfeng1220@gmail.com>
2018-06-19 14:31:46 -0700

    lois

---
+++ project.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var checkbox = document.getElementById("instant");
+    var hotpot = document.getElementById("hotpot");
+    if (checkbox.checked == true){
+        hotpot.style.display = "block";
+    } else {
+       hotpot.style.display = "none";
+    }
+}
+
+function afunction(){
+  var fries = document.getElementById("spam")
+  var friesa =document.getElementById("musubi")
+  if(fries.checked == true){
+    friesa.style.display = "block";
+  }else{
+    friesa.style.display = "none";
+  }
+}
+
+function rofunction(){
+  var rose=document.getElementById("rose")
+  var tea=documnet.getElementById("tea")
+  if(checkbox.checked == true){
+    tea.style.display
+  }
+}
