fe3e18340a3f741edfebbef8dfa63bbb4cbd6f40
unknown <michaelwei0606@gmail.com>
2018-06-20 13:38:42 -0700

    f

---
+++ project.js
@@ -0,0 +1,269 @@
+function myFunction() {
+    var checkbox = document.getElementById("instant");
+    var hotpot = document.getElementById("hotpot");
+    if (checkbox.checked == true){
+        hotpot.style.display = "block";
+    } else {
+       hotpot.style.display = "none";
+    }
+}
+
+function afunction(){
+  var fries = document.getElementById("spam")
+  var friesa =document.getElementById("musubi")
+  if(fries.checked == true){
+    friesa.style.display = "block";
+  }else{
+    friesa.style.display = "none";
+  }
+}
+
+function rofunction(){
+  var rose =document.getElementById("rose")
+  var tea =document.getElementById("tea")
+  if(rose.checked == true){
+    tea.style.display = "block";
+  }else{
+    tea.style.display = "none";
+  }
+}
+
+function infunction(){
+  var instant =document.getElementById("instant")
+  var noodle =document.getElementById("noodle")
+  if(instant.checked == true){
+    noodle.style.display = "block";
+  }else{
+    noodle.style.display = "none";
+  }
+}
+
+function icfunction(){
+  var ice =document.getElementById("ice")
+  var cream =document.getElementById("cream")
+  if(ice.checked == true){
+    cream.style.display = "block";
+  }else{
+    cream.style.display = "none";
+  }
+}
+
+function pafunction(){
+  var packed =document.getElementById("packed")
+  var meals =document.getElementById("meals")
+  if(packed.checked == true){
+    meals.style.display = "block";
+  }else{
+    meals.style.display = "none";
+  }
+}
+
+function insafunction(){
+  var instanta =document.getElementById("instanta")
+  var noodlesa=document.getElementById("noodlesa")
+  if(instanta.checked == true){
+    noodlesa.style.display = "block";
+  }else{
+    noodlesa.style.display = "none";
+  }
+}
+
+function hafunction(){
+  var hardboiled =document.getElementById("hardboiled")
+  var eggs =document.getElementById("eggs")
+  if(hardboiled.checked == true){
+    eggs.style.display = "block";
+  }else{
+    eggs.style.display = "none";
+  }
+}
+
+function saufunction(){
+  var sau =document.getElementById("sau")
+  var sages =document.getElementById("sages")
+  if(sau.checked == true){
+    sages.style.display = "block";
+  }else{
+    sages.style.display = "none";
+  }
+}
+
+function rifunction(){
+  var rice = document.getElementById("rice")
+  var roll= document.getElementById("roll")
+  if(rice.checked == true){
+    roll.style.display ="block"
+  }else{
+    roll.style.display ="none"
+  }
+}
+
+function frifunction(){
+  var fried= document.getElementById("fried")
+  var chicken= document.getElementById("chicken")
+  if(fried.checked == true){
+    chicken.style.display ="block"
+  }else{
+    chicken.style.display ="none"
+  }
+}
+
+function spamafunction(){
+  var spama= document.getElementById("spama")
+  var musubia= document.getElementById("musubia")
+  if(spama.checked == true){
+    musubia.style.display ="block"
+  }else{
+    musubia.style.display ="none"
+  }
+}
+
+function iceafunction(){
+  var icea = document.getElementById("icea")
+  var creama = document.getElementById("creama")
+  if(icea.checked == true){
+    creama.style.display ="block"
+  }else{
+    creama.style.display ="none"
+  }
+}
+
+function instantbfunction(){
+  var instantb= document.getElementById("instantb")
+  var hotpotb= document.getElementById("hotpotb")
+  if(instantb.checked == true){
+    hotpotb.style.display ="block"
+  }else{
+    hotpotb.style.display ="none"
+  }
+}
+
+function frfunction(){
+  var french= document.getElementById("french")
+  var fries= document.getElementById("fries")
+  if(french.checked == true){
+    fries.style.display ="block"
+  }else{
+    fries.style.display ="none"
+  }
+}
+
+function pofunction(){
+  var popcorn= document.getElementById("popcorn")
+  var chickens= document.getElementById("chickens")
+  if(popcorn.checked == true){
+    chickens.style.display ="block"
+  }else{
+    chickens.style.display ="none"
+  }
+}
+
+function packedafunction(){
+  var packeda= document.getElementById("packeda")
+  var mealsa= document.getElementById("mealsa")
+  if(packeda.checked == true){
+    mealsa.style.display ="block"
+  }else{
+    mealsa.style.display ="none"
+  }
+}
+
+function fifunction(){
+  var fish= document.getElementById("fish")
+  var cakes= document.getElementById("cakes")
+  if(fish.checked == true){
+    cakes.style.display ="block"
+  }else{
+    cakes.style.display ="none"
+  }
+}
+
+function wafunction(){
+  var wa= document.getElementById("wa")
+  var ter= document.getElementById("ter")
+  if(wa.checked == true){
+    ter.style.display ="block"
+  }else{
+    ter.style.display ="none"
+  }
+}
+
+function cofunction(){
+  var cof= document.getElementById("cof")
+  var fee= document.getElementById("fee")
+  if(cof.checked == true){
+    fee.style.display ="block"
+  }else{
+    fee.style.display ="none"
+  }
+}
+
+function misfunction(){
+  var milky= document.getElementById("milky")
+  var way= document.getElementById("way")
+  if(milky.checked == true){
+    way.style.display ="block"
+  }else{
+    way.style.display ="none"
+  }
+}
+
+function blfunction(){
+  var black= document.getElementById("black")
+  var teaa= document.getElementById("teaa")
+  if(black.checked == true){
+    teaa.style.display ="block"
+  }else{
+    teaa.style.display ="none"
+  }
+}
+
+function grfunction(){
+  var green= document.getElementById("green")
+  var teab= document.getElementById("teab")
+  if(green.checked == true){
+    teab.style.display ="block"
+  }else{
+    teab.style.display ="none"
+  }
+}
+
+function jafunction(){
+  var jasmine= document.getElementById("jasmine")
+  var teac= document.getElementById("teac")
+  if(jasmine.checked == true){
+    teac.style.display ="block"
+  }else{
+    teac.style.display ="none"
+  }
+}
+
+function rosebfunction(){
+  var roseb = document.getElementById("roseb")
+  var tead= document.getElementById("tead")
+  if(roseb.checked == true){
+    tead.style.display ="block"
+  }else{
+    tead.style.display ="none"
+  }
+}
+
+function milkafunction(){
+  var milka = document.getElementById("milka")
+  var milkd= document.getElementById("milkd")
+  if(milka.checked == true){
+    milkd.style.display ="block"
+  }else{
+    milkd.style.display ="none"
+  }
+}
+
+function yofunction(){
+  var yo= document.getElementById("yo")
+  var gurt= document.getElementById("gurt")
+  if(yo.checked == true){
+    gurt.style.display ="block"
+  }else{
+    gurt.style.display ="none"
+  }
+}
